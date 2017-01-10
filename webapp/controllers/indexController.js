b785dcf87395758c94578f6a870add3b284bf82c
Raphael Fernandes <raphael.cardoso.f@gmail.com>
2017-01-10 13:36:58 -0200

    controller do index resolvido

--- webapp/controllers/indexController.js
+++ webapp/controllers/indexController.js
@@ -1,4 +1,4 @@
-angular.module('myApp', []).controller('namesCtrl', function($scope) {
+angular.module('index', []).controller('indexCtrl', function($scope) {
     $scope.names = [
         {name:'Jani',country:'Norway'},
         {name:'Hege',country:'Sweden'},
